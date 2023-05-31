eaf348c0829701c0c7ee0068fafc3b9e09574de0
Amal <connecttoamalmathew@gmail.com>
2023-05-31 13:14:52 +0530

    views updated

--- myApp/views.py
+++ myApp/views.py
@@ -8,11 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 def json_data_view(request):
     if request.method == 'POST':
         data = json.loads(request.body)  # Assuming you receive the JSON data in the request body
-        serializer = MyModelSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse({'message': 'Data saved successfully.'})
-        else:
-            return JsonResponse(serializer.errors, status=400)
+        my_model = MyModel(json_file=data)
+        my_model.save()
+        return JsonResponse({'message': 'Data saved successfully.'})
     else:
         return JsonResponse({'message': 'Invalid request method.'})
