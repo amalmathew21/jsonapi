00103c57f3580aa14e5f2b25c7a7a4b328da327b
Amal Mathew <connecttoamalmathew@gmail.com>
2023-05-31 14:44:22 +0530

    Update settings.py

--- ApiJson/settings.py
+++ ApiJson/settings.py
@@ -154,7 +154,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'https://jsonexample.onrender.com/'
+    'https://jsonexample.onrender.com',
 
     'http://127.0.0.1:3000',
     'https://localhost:3000',
