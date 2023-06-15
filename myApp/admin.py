869b92c05dcdd6648330f6ad69b8949496c359b9
Amal <connecttoamalmathew@gmail.com>
2023-06-15 13:21:54 +0530

    new Models added

--- myApp/admin.py
+++ myApp/admin.py
@@ -4,8 +4,13 @@ from .models import *
 # Register your models here.
 
 admin.site.register(MyModel)
-admin.site.register(ImageModel)
+admin.site.register(DropdownModel)
 admin.site.register(DataModel)
-admin.site.register(Leads)
+admin.site.register(Lead)
+admin.site.register(Accounts)
+admin.site.register(Opportunities)
+admin.site.register(Task)
+admin.site.register(Report)
+admin.site.register(Notes)
 
 
